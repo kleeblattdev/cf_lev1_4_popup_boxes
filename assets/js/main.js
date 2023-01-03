f19dfd3171b552edbca7cac912c202a380de92f2
karinlee <karinlee92@gmail.com>
2023-01-03 14:28:47 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+window.alert("Hallo Welt");
+window.prompt("Bitte geben Sie Ihren Namen ein.");
+window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+window.confirm("Stimmen Sie meiner Meinung zu?"); 
+
+let alter = prompt( "Gib mir bitte dein Alter:" );
+console.log(alter);
+
+let b = 5;
+let a = b * 5 -3;
+alert(a);
+
+let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+let grosse = "2,72m";
+alert(mann + grosse);
+
+/* window.alert takes the focus away from the current window, and the user has to cose the box to access to the other parts of the page */
+
+/*window.prompt is used if you want the user to input a value */
+
+/*window.confirm is used if you want the user to verify or accept something */
